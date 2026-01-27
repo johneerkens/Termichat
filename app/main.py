0881ade29e103f739044d2e7b9d1639059c0fbbf
John Eerkens <johneerkens@hotmail.com>
2026-01-27 21:13:09 +0100

    adjust code

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
-from chat import start_chat
-
 def main():
     print("🤖 TermiChat — Linux Terminal AI")
     print("Type 'exit' to quit\n")
-    start_chat()
 
-if __name__ == "__main__":
-    main()
+    from chat import start_chat
+    start_chat()
