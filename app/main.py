d16e5964edd10938861444d8f3880d4cc6e369a3
johneerkens <johneerkens@hotmail.com>
2026-01-27 13:58:27 +0100

    added code in main.py and text in requirements.txt

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from chat import start_chat
+
+def main():
+    print("🤖 TermiChat — Linux Terminal AI")
+    print("Type 'exit' to quit\n")
+    start_chat()
+
+if __name__ == "__main__":
+    main()
